c8ee369c7f27e3f05f8b5229ea4be6413b0aeb87
nareshsukheja12 <nareshsukheja12@gmail.com>
2024-05-26 15:21:50 +0530

    feat : add route for Register component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Register from './components/Register';
+import bgLeft from './assets/bg-left.png';
+import bgRight from './assets/bg-right.png';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="min-h-screen bg-[#f6f2eb] flex items-center justify-center">
+        <div className="absolute inset-y-0 z-0" style={{ backgroundImage: `url(${bgLeft})` }}></div>
+        <div className="absolute inset" style={{ backgroundImage: `url(${bgRight})` }}></div>
+        <Routes>
+          <Route path="/" element={<Register />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
